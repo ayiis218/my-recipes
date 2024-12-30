00fbe2eea398aed65e09008c48dcd9272c786a96
Ayi Solahudin <ayiis@MacBook-Air-Ayi.local>
2024-12-30 15:15:15 +0700

    create spesifik page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+'use server'
+
+import AboutUs from "@/components/pages/about/page"
+
+export default async function PageAbout() {
+   return <AboutUs />
+}
---
+++ src/components/pages/about/page.tsx
@@ -0,0 +1,11 @@
+'use server'
+
+import Typography from "@/components/atoms/typography"
+
+export default async function AboutUs() {
+   return (
+      <div className="w-full h-100 flex flex-col items-center ">
+         <Typography variant="h1">Hello Guys in About Us</Typography>
+      </div>
+   )
+}
